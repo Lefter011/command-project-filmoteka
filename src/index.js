d89f43e065645fadb0144514527db1a1a8a893c3
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-10-28 21:12:51 +0300

    no-scroll
    disabled body scroll when modal open

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/utils/handlebars-helpers';
-import './js/modal-card-film/modal';
+import './js/modal-card-film/modal-refs';
+import './js/modal-card-film/modal-open';
 // import { localStorageFilms } from
--- src/js/modal-card-film/modal-refs.js
+++ src/js/modal-card-film/modal-refs.js
@@ -3,11 +3,14 @@ const refs = {
   openBtn: document.querySelector('button[data-modal-open]'),
   closeBtn: document.querySelector('button[data-modal-close]'),
   backdrop: document.querySelector('div[data-modal]'),
+  modal: document.querySelector('div.js-movie-card'),
 };
 
 refs.openBtn.addEventListener('click', onOpenBtnClick);
 function onOpenBtnClick() {
   refs.backdrop.classList.remove('is-hidden');
+  refs.modal.classList.add('is-open');
+  refs.body.classList.add('modal-shown');
 }
 
 refs.closeBtn.addEventListener('click', onCloseBtnClick);
