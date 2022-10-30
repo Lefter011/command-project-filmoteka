d325595a83aea25b2d3f5fb40f08cd24aa3a8666
lady-araminta <jparkhomenko@gmail.com>
2022-10-30 15:03:20 +0200

    Update header-search.js

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -20,6 +20,7 @@ async function onSubmit(event) {
       'Search result is unsuccessful. Enter the correct movie name and try again!'
     );
   }
+  clearMarkup();
   const markup = createMarkup(data);
   containerGallery.insertAdjacentHTML('beforeend', markup);
 }
@@ -30,12 +31,16 @@ function createMarkup(data) {
     .map(({ poster_path, original_title, genre_ids, release_date }) => {
       const releaseYear = release_date.substring(0, 4);
       const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
+      let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
+      if (poster_path === null) {
+        posterLink = 'https://i.postimg.cc/zG4yJ7P4/No-Image-Available.jpg';
+      }
       return `<li class="hero__movie-item">
 <div class="gallery__section">
     <ul class="gallery__card">
         <li class="gallery-list list">
             <div class="gallary__titular">
-                <img src="${BASE_PICTURE_URL}${poster_path}" width="280" alt="${original_title}" class="gallery__img">
+                <img src="${posterLink}" alt="${original_title}" class="gallery__img">
         </li>
         </div>
         <div class="gallary__info">
@@ -47,4 +52,8 @@ function createMarkup(data) {
 </li>`;
     })
     .join('');
+}
+
+function clearMarkup() {
+  containerGallery.innerHTML = '';
 }
