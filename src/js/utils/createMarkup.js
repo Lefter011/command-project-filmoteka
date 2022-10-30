69c6d5f96183a52f24fe95a4eff6258fdddf382d
Anakole <104356239+Anakole@users.noreply.github.com>
2022-10-31 00:44:10 +0200

    edits

--- src/js/utils/createMarkup.js
+++ src/js/utils/createMarkup.js
@@ -1,12 +1,19 @@
 import { urls } from '../utils/config';
+import { getGenres } from './getGenres';
 
 export function createMarkup(movies) {
   return movies
-    .map(({ genre_ids, title, poster_path, release_date }) => {
-      return `<li class="card__item">
+    .map(({ id, genre_ids, title, poster_path, release_date }) => {
+      const releaseYear = release_date.substring(0, 4);
+      const genresList = getGenres(genre_ids, 3);
+      let posterLink = `${urls.BASE_IMAGE_URL}${poster_path}`;
+      if (poster_path === null) {
+        posterLink = 'https://i.postimg.cc/zG4yJ7P4/No-Image-Available.jpg';
+      }
+      return `<li class="card__item" data-id=${id}>
   <div class="card__img-wrap">
     <img
-      src="${urls.BASE_IMAGE_URL}${poster_path}"
+      src="${posterLink}"
       alt="${title}"
       class="card__img"
     />
@@ -14,8 +21,8 @@ export function createMarkup(movies) {
   <div class="card__text-wrap">
     <h2 class="card__name">${title}</h2>
     <div class="card__info">
-      <p class="card__genres">Drama, Action<span class="card__year"></span></p>
-      <p class="card__year">2022</p>
+      <p class="card__genres">${genresList}<span class="card__year"></span></p>
+      <p class="card__year">${releaseYear}</p>
     </div>
   </div>
 </li>`;
