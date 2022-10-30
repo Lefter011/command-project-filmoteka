290606c8d63142b38419abd676d47879e1dc8339
lady-araminta <104200584+lady-araminta@users.noreply.github.com>
2022-10-30 15:11:04 +0200

    Header/search (#21)
    * Add search bu name
    
    * Update ApiService.js
    
    * Add function createMarkup
    
    * Update header-search.js
    
    Co-authored-by: Mike Chernetskyi <94438982+Lefter011@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import './js/utils/handlebars-helpers';
 import './js/footer_modal';
 // import { localStorageFilms } from
 import './js/modal-card-film/modal';
+import './js/header-search';
 import './js/hero';
 import './js/utils/set-genres-to-localstorage';
+
 import './js/pagination';
 
 import './js/utils/loc-storage';
---
+++ src/js/header-search.js
@@ -0,0 +1,59 @@
+import { containerGallery, searchFormRef } from './utils/refs';
+import ApiService from './ApiService';
+
+const api = new ApiService();
+
+searchFormRef.addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const searchQuery = event.currentTarget.elements.searchQuery.value
+    .trim()
+    .toLowerCase();
+  if (!searchQuery) {
+    alert('Enter a search query!');
+    return;
+  }
+  const data = await api.getMoviesByName(searchQuery);
+  if (data.total_results === 0) {
+    alert(
+      'Search result is unsuccessful. Enter the correct movie name and try again!'
+    );
+  }
+  clearMarkup();
+  const markup = createMarkup(data);
+  containerGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function createMarkup(data) {
+  const { results } = data;
+  return results
+    .map(({ poster_path, original_title, genre_ids, release_date }) => {
+      const releaseYear = release_date.substring(0, 4);
+      const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
+      let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
+      if (poster_path === null) {
+        posterLink = 'https://i.postimg.cc/zG4yJ7P4/No-Image-Available.jpg';
+      }
+      return `<li class="hero__movie-item">
+<div class="gallery__section">
+    <ul class="gallery__card">
+        <li class="gallery-list list">
+            <div class="gallary__titular">
+                <img src="${posterLink}" alt="${original_title}" class="gallery__img">
+        </li>
+        </div>
+        <div class="gallary__info">
+            <p class="card__name">${original_title}</p>
+            <p class="card__info">${genre_ids} , ${releaseYear}</p>
+        </div>
+    </ul>
+</div>
+</li>`;
+    })
+    .join('');
+}
+
+function clearMarkup() {
+  containerGallery.innerHTML = '';
+}
