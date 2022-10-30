0bc324a7b0ac9a43cbebbb6dd438aa9c581d74b2
Anakole <104356239+Anakole@users.noreply.github.com>
2022-10-30 15:04:27 +0200

    Hero trends (#24)
    * Update config.js
    
    * add movies to the hero
    
    * Delete _filmCard.scss
    
    * Update _hero.scss
    
    Co-authored-by: Mike Chernetskyi <94438982+Lefter011@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './js/utils/handlebars-helpers';
 
 import './js/footer_modal';
-// import { localStorageFilms } from 
+// import { localStorageFilms } from
 import './js/modal-card-film/modal';
+import './js/hero';
 import './js/utils/set-genres-to-localstorage';
 import './js/pagination'
