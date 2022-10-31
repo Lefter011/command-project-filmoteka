3a1a2b78e9c5cfdf76a56e3782cbbd1be09cfc8e
AndreyNik2 <105128068+AndreyNik2@users.noreply.github.com>
2022-10-31 13:25:29 +0200

    Edit watched/queue list (#33)
    * 1th
    
    * complete my library
    
    Co-authored-by: Mike Chernetskyi <94438982+Lefter011@users.noreply.github.com>

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,2 +1,7 @@
 import './js/utils/handlebars-helpers';
 import "./js/utils/set-genres-to-localstorage"
+import "./js/utils/getGenresName"
+import './js/random-films'
+import "./js/my-library-create-murkup"
+
+
