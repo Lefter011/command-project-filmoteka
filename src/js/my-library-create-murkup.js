821691473a54c4be08f4ddbd58a83db4c418702f
AndreyNik2 <dark.tm2011@gmail.com>
2022-10-31 12:58:46 +0200

    complete my library

--- src/js/my-library-create-murkup.js
+++ src/js/my-library-create-murkup.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { parse } from "handlebars";
 import { API_KEY } from "./utils/config"
 import { urls } from "./utils/config";
+import { getGenresName } from "./utils/getGenresName";
 
 
 
@@ -17,10 +18,32 @@ function onLoadPage() {
     queueBTN.classList.remove('active-btn');
     watchedBTN.classList.add('active-btn');
     watchedBTN.classList.remove('inactive-btn');
+    myLibrary.style.height = 'auto';
     myLibraryList.insertAdjacentHTML("beforeend", createMarkupWatched())
-
-
 }
+
+function onWatchedClick() {
+    clearPage();
+    queueBTN.classList.add('inactive-btn');
+    queueBTN.classList.remove('active-btn');
+    watchedBTN.classList.add('active-btn');
+    watchedBTN.classList.remove('inactive-btn');
+    myLibrary.style.height = 'auto';
+    myLibraryList.insertAdjacentHTML("beforeend", createMarkupWatched())
+}
+
+function onQueueClick() {
+    clearPage();
+    queueBTN.classList.add('active-btn');
+    queueBTN.classList.remove('inactive-btn');
+    watchedBTN.classList.add('inactive-btn');
+    watchedBTN.classList.remove('active-btn');
+    myLibrary.style.height = 'auto';
+    myLibraryList.insertAdjacentHTML("beforeend", createMarkupQueue())
+}
+
+watchedBTN.addEventListener('click', onWatchedClick)
+queueBTN.addEventListener('click', onQueueClick)
 
 onLoadPage();
 
@@ -35,8 +58,9 @@ function clearPage() {
 
 function createMarkupWatched() {
     const watchedMovies = JSON.parse(localStorage.getItem("watched"));
-    if (watchedMovies.length === 20) {
+    if (!watchedMovies) {
         myLibrary.style.height = '100vh';
+        myLibraryList.textContent = "";
         myLibraryList.textContent = "Sorry you don't have any movies added. Create a new list now";
         return
     }
@@ -44,6 +68,8 @@ function createMarkupWatched() {
     .map(({ poster_path, original_title, genre_ids, release_date, vote_average }) => {
         const releaseYear = release_date.substring(0, 4);
         const vote = Number(vote_average).toFixed(1);
+        const genres = getGenresName(genre_ids);
+        const slicedGenres = sliseGenres(genres)
       const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
       let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
       if (poster_path === null) {
@@ -55,10 +81,11 @@ function createMarkupWatched() {
                 class="mylibrary__item-img"
                 src="${posterLink}"
                 alt="${original_title}"
+                loading=lazy
             />
             <h2 class="mylibrary__item-title">${original_title}</h2>
             <div class="mylibrary__item-text-wrapper">
-                <p class="mylibrary__item-genres">${genre_ids}</p>
+                <p class="mylibrary__item-genres">${slicedGenres.join(", ")}</p>
                 <p class="mylibrary__item-text-separator">|</p>
                 <p class="mylibrary__item-release-arye">${releaseYear}</p>
                 <div class="reting-wrapper">
@@ -72,12 +99,20 @@ function createMarkupWatched() {
 }
 
 function createMarkupQueue() {
-    const WatchedMovies = JSON.parse(localStorage.getItem("queue"));
+    const queueMovies = JSON.parse(localStorage.getItem("queue"));
+     if (!queueMovies) {
+        myLibrary.style.height = '100vh';
+        myLibraryList.textContent = "";
+        myLibraryList.textContent = "Sorry you don't have any added movies. Create a new list now";
+        return
+    }
     
-  return WatchedMovies
+  return queueMovies
     .map(({ poster_path, original_title, genre_ids, release_date, vote_average }) => {
         const releaseYear = release_date.substring(0, 4);
         const vote = Number(vote_average).toFixed(1);
+        const genres = getGenresName(genre_ids);
+        const slicedGenres = sliseGenres(genres)
       const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
       let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
       if (poster_path === null) {
@@ -89,10 +124,11 @@ function createMarkupQueue() {
                 class="mylibrary__item-img"
                 src="${posterLink}"
                 alt="${original_title}"
+                loading=lazy
             />
             <h2 class="mylibrary__item-title">${original_title}</h2>
             <div class="mylibrary__item-text-wrapper">
-                <p class="mylibrary__item-genres">${genre_ids}</p>
+                <p class="mylibrary__item-genres">${slicedGenres}</p>
                 <p class="mylibrary__item-text-separator">|</p>
                 <p class="mylibrary__item-release-arye">${releaseYear}</p>
                 <div class="reting-wrapper">
@@ -104,6 +140,16 @@ function createMarkupQueue() {
     })
     .join('');
 }
+function sliseGenres(genres) { 
+    
+    if (genres.length < 3) {
+        
+        return genres
+    } else {
+        return [...genres.slice(0, 2), "Other"];
+        
+    }
+};
 
 
 
---
+++ src/js/utils/getGenresName.js
@@ -0,0 +1,11 @@
+export function getGenresName(movieID) {
+    const genresID = JSON.parse(localStorage.getItem('genres'));
+    return movieID.reduce((acc, item) => {
+        const findGenre = genresID.find(el => Number(el.id) === Number(item));
+        return findGenre ? [...acc, findGenre.name] : acc;
+    }, []);
+}
+
+
+
+
