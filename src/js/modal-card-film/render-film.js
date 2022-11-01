c1aefcedae41c85c9d62b6db483e875466735a09
AndreyNik2 <dark.tm2011@gmail.com>
2022-11-01 11:43:58 +0200

    fix function import

--- src/js/modal-card-film/render-film.js
+++ src/js/modal-card-film/render-film.js
@@ -2,7 +2,7 @@ import { refs } from './modal-refs';
 import { films } from './random-films';
 const Handlebars = require('handlebars');
 import { getGenresName } from '../utils/get-genres-name';
-import { sliseGenres } from '../my-library-create-murkup';
+import { sliceGenres } from '../utils/slice-genres';
 
 const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/';
 const desktopSize = 'original';
@@ -22,7 +22,7 @@ export function createModalCardMarkup(film) {
   } = film;
 
   const genresIds = genres.map(genre => genre.id);
-  const cutGenres = sliseGenres(genresIds);
+  const cutGenres = sliceGenres(genresIds);
   const genresNames = getGenresName(cutGenres).join(', ');
   const poster = poster_path
     ? '${BASE_PICTURE_URL}${mobileSize}${poster_path}'
--- src/js/my-library-create-murkup.js
+++ src/js/my-library-create-murkup.js
@@ -1,8 +1,5 @@
-import axios from 'axios';
-import { parse } from 'handlebars';
-import { API_KEY } from './utils/config';
-import { urls } from './utils/config';
-import { getGenresName } from './utils/get-genres-name';
+import { getGenresName } from "./utils/get-genres-name";
+import { sliceGenres } from "./utils/slice-genres";
 
 const queueBTN = document.querySelector('.queue-btn');
 const watchedBTN = document.querySelector('.watched-btn');
@@ -70,7 +67,7 @@ function createMarkupWatched() {
         const releaseYear = release_date.substring(0, 4);
         const vote = Number(vote_average).toFixed(1);
         const genres = getGenresName(genre_ids);
-        const slicedGenres = sliseGenres(genres);
+        const slicedGenres = sliceGenres(genres);
         const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
         let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
         if (poster_path === null) {
@@ -122,7 +119,7 @@ function createMarkupQueue() {
         const releaseYear = release_date.substring(0, 4);
         const vote = Number(vote_average).toFixed(1);
         const genres = getGenresName(genre_ids);
-        const slicedGenres = sliseGenres(genres);
+        const slicedGenres = sliceGenres(genres);
         const BASE_PICTURE_URL = 'https://image.tmdb.org/t/p/original';
         let posterLink = `${BASE_PICTURE_URL}${poster_path}`;
         if (poster_path === null) {
