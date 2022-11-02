4beeb0768172934171cf97b4148bf811b6d32a5b
AndreyNik2 <dark.tm2011@gmail.com>
2022-11-02 16:59:47 +0200

    Fix-my-library for getLocalstorage

--- src/js/utils/get-genres-name.js
+++ src/js/utils/get-genres-name.js
@@ -1,5 +1,8 @@
 export function getGenresName(movieID) {
     const genresID = JSON.parse(localStorage.getItem('genres'));
+    if (!movieID) {
+        return
+    }
     return movieID.reduce((acc, item) => {
         const findGenre = genresID.find(el => Number(el.id) === Number(item));
         return findGenre ? [...acc, findGenre.name] : acc;
