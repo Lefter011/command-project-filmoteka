3e4c9d68103ea2733564d63b6437b6e0d4a20ace
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-11-04 00:24:06 +0200

    theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -7,11 +7,12 @@ const Theme = {
 
 const body = document.querySelector("body");
 const footer = document.querySelector("footer");
-// const modal = document.querySelector(".modal-wrap");
+;
 
 const delClassElem = () => {
   body.classList.remove(Theme.LIGHT, Theme.DARK);
   footer.classList.remove(Theme.LIGHT, Theme.GREY);
+
 };
 const themeSwitcher = document.querySelector("#theme-switch-toggle");
 
@@ -21,16 +22,17 @@ themeSwitcher.addEventListener("change", () => {
     localStorage.setItem("Theme", "darkTheme");
     body.classList.add(Theme.DARK);
     footer.classList.add(Theme.GREY);
-    // modal.classList.add(Theme.GREY);
+
   } else {
     localStorage.setItem("Theme", "lightTheme");
     body.classList.add(Theme.LIGHT);
+  
   }
 });
 if (localStorage.getItem("Theme") === "darkTheme") {
   themeSwitcher.setAttribute("checked", true);
   body.classList.add(Theme.DARK);
   footer.classList.add(Theme.GREY);
-//   modal.classList.add(Theme.GREY);
+
 }
 
