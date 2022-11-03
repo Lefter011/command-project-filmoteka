76d9e7df8a9983f05c63303d950765c3103c00e1
Anakole <104356239+Anakole@users.noreply.github.com>
2022-11-03 20:00:33 +0200

    add styles

--- src/js/new-team-modal.js
+++ src/js/new-team-modal.js
@@ -8,6 +8,7 @@ const refs = {
   body: document.querySelector('body'),
   teamList: document.querySelector('.footer__team-list'),
   memberCard: document.querySelector('.footer__member-card'),
+  title: document.querySelector('.new-footer__title'),
 };
 
 (() => {
@@ -18,12 +19,15 @@ const refs = {
   function toggleTeamModal() {
     refs.teamModal.classList.toggle('visually-hidden');
     refs.body.classList.toggle('no-scroll');
+    refs.memberCard.innerHTML = '';
   }
 
   function backdropClick(evt) {
     const target = evt.target.className;
     if (target === 'new-footer__modal-backdrop') {
       refs.teamModal.classList.add('visually-hidden');
+      refs.memberCard.innerHTML = '';
+      refs.title.classList.remove('visually-hidden');
     }
   }
 
@@ -32,11 +36,13 @@ const refs = {
     function onTeamMemberClick(evt) {
       evt.preventDefault();
       refs.memberCard.innerHTML = '';
+      refs.title.classList.remove('visually-hidden');
       try {
         const memberId = evt.target.parentNode.dataset.id;
         const memberData = teamArray.find(member => member.id === memberId);
         const markup = createCentralCardMarkup(memberData);
         refs.memberCard.insertAdjacentHTML('beforeend', markup);
+        refs.title.classList.add('visually-hidden');
       } catch (error) {
         console.log(error);
       }
@@ -53,10 +59,9 @@ const refs = {
 
 function createTeamMarkup(team) {
   return team
-    .map(({ username, image, id }) => {
+    .map(({ image, id }) => {
       return `<li class="footer__team-item" data-id=${id}>
   <img src="${image}" alt="image of a member" class="footer__team-img" />
-  <p class="footer__team-name">${username}</p>
 </li>`;
     })
     .join('');
@@ -72,21 +77,21 @@ function createCentralCardMarkup(member) {
     <li class="footer__soc-item">
       <a href="${github}" class="footer__soc-link">
         <svg>
-          <use href=""></use>
+          <use href="./image/soc-icons.svg#icon-github"></use>
         </svg>
       </a>
     </li>
     <li class="footer__soc-item">
       <a href="${linkedin}" class="footer__soc-link">
         <svg>
-          <use href=""></use>
+          <use href="./image/soc-icons.svg#icon-linkedin2"></use>
         </svg>
       </a>
     </li>
     <li class="footer__soc-item">
       <a href="${facebook}" class="footer__soc-link">
         <svg>
-          <use href=""></use>
+          <use href="./image/soc-icons.svg#icon-facebook"></use>
         </svg>
       </a>
     </li>
